9bcee83639539c49d590c877db1a723e99c502d7
Suyog Kalyan Nagawade <92072200+Suyog8383@users.noreply.github.com>
2023-05-07 19:51:28 +0530

    Create errorInJavaScript.js

---
+++ errorInJavaScript.js
@@ -0,0 +1,13 @@
+//Reference Error
+let firstName = 'Asabeneh'
+let fullName = firstName + ' ' + lastName
+console.log(fullName);
+
+//Syntax Error
+let square = 2 x 2
+console.log(square)
+console.log('Hello, world")
+
+//Type Error
+let num = 10
+console.log(num.toLowerCase());
